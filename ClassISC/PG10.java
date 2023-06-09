96590e208c8d9c36fab6b811bbf756817557de75
PredatorZ59 <tirtheshdhumalz59@gmail.com>
2023-06-09 18:26:00 +0530

    new files

--- ClassISC/PG10.java
+++ ClassISC/PG10.java
@@ -8,7 +8,7 @@ public class PG10 {
 		Scanner ti=new Scanner(System.in);
 		System.out.println("Enter the Length:");
 		int l=ti.nextInt();
-		System.out.println("Enter the Breadth:");
+		System.out.println("Enter the Breadth:");//hi
 		int b=ti.nextInt();
 		  int area=l*b;
 		  System.out.println("The Area of rectrangle is : "+area);
