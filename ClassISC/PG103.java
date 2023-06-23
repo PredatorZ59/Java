e3785c601d7964bf1f46a8d55d1639f92320572f
PredatorZ59 <tirtheshdhumalz59@gmail.com>
2023-06-23 17:40:34 +0530

    new

---
+++ ClassISC/PG103.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class PG103 {
+
+    static void Display(){
+        System.out.println("This is an Example of DispalyMethod.");
+    }
+
+    public static void main(String[] args) {
+        Display();
+    }
+    
+}
