8af17eb88e310101cf1513d95551f9b1b408a105
PredatorZ59 <tirtheshdhumalz59@gmail.com>
2023-06-30 17:20:32 +0530

    New

---
+++ Practice/P1.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class P1 {
+    public static void main(String[] args) {
+        
+        String p1="Tirthesh";
+        String p2="Tirthesh";
+        String p3="Sarvesh";
+        System.out.println(p1.equals(p2)); 
+        System.out.println(p1.equals(p1));
+        System.out.println(p1.equals(p3));
+    }
+}
