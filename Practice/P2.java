fafa20a257e2ae72bd922e0a673f644a218f6f63
PredatorZ59 <tirtheshdhumalz59@gmail.com>
2023-06-30 17:33:37 +0530

    new

--- Practice/P2.java
+++ Practice/P2.java
@@ -3,7 +3,12 @@ package Practice;
 public class P2 {
     public static void main(String[] args) {
         
-        
+        String p1="Tirthesh";
+        String p2="Tirthesh";
+        String p3="Sarvesh";
+        System.out.println(p1.compareTo(p2));
+        System.out.println(p1.compareTo(p3));
+        System.out.println(p3.compareTo(p1));
     }
     
 }
