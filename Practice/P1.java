e9a06f8e410fd0dd4d75370a30420766bf18f7b6
PredatorZ59 <tirtheshdhumalz59@gmail.com>
2023-06-30 17:25:44 +0530

    new

--- Practice/P1.java
+++ Practice/P1.java
@@ -6,8 +6,8 @@ public class P1 {
         String p1="Tirthesh";
         String p2="Tirthesh";
         String p3="Sarvesh";
-        System.out.println(p1.equals(p2));//true 
-        System.out.println(p1.equals(p1));//true
-        System.out.println(p1.equals(p3));//false
+        System.out.println(p1.equals(p2));//true // OR (p1==p2);
+        System.out.println(p1.equals(p1));//true // OR (p2==p3);
+        System.out.println(p1.equals(p3));//false // OR (p1==p3);
     }
 }
---
+++ Practice/P2.java
@@ -0,0 +1,9 @@
+package Practice;
+
+public class P2 {
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
