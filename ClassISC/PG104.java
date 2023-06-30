1ee79d4a1b6f43eddda635cbc29e17c0070ba845
PredatorZ59 <tirtheshdhumalz59@gmail.com>
2023-06-30 17:11:40 +0530

    New

---
+++ ClassISC/PG104.java
@@ -0,0 +1,11 @@
+public class PG104{
+    public static void main(String[] args) {
+        PG104 obj =new PG104();
+        System.out.println("The sum is"+obj.add(12,13));
+    }
+      int s;
+    public int add(int a,int b){
+        s=a+b;
+        return s;
+    }
+}
