ba2efd4e92cf362c4bda80aa7784ee5f5b344f3f
PredatorZ59 <tirtheshdhumalz59@gmail.com>
2023-07-21 17:15:45 +0530

    new

--- ClassISC/PG109.java
+++ ClassISC/PG109.java
@@ -6,6 +6,6 @@ public class PG109 {
     public static void main(String[] args) {
         System.out.println(A.add(11,11));
         System.out.println(A.add(11,11,11));
-        //ygeygrgegg
+     
     }    
 }
