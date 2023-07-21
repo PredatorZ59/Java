cd42eaf651219b039e8bd0ca16d4bf1f8492e1e9
PredatorZ59 <tirtheshdhumalz59@gmail.com>
2023-07-21 17:12:40 +0530

    New

---
+++ ClassISC/PG109.java
@@ -0,0 +1,10 @@
+class A{
+    static int add(int a, int b){return a+b;}
+    static int add(int a, int b, int c){return a+b+c;}
+}
+public class PG109 {
+    public static void main(String[] args) {
+        System.out.println(A.add(11,11));
+        System.out.println(A.add(11,11,11));
+    }    
+}
